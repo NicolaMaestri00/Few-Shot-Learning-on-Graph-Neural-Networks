41b0a65e5b2cfe5bbfc0171cfe6eb36556e24f1b
Nicola Maestri <nicola.maestri00@gmail.com>
2025-04-27 18:19:51 +0200

    Refactored node_classification

--- scripts/node_classification/node_classification.py
+++ scripts/node_classification/node_classification.py
@@ -18,34 +18,34 @@ if __name__ == "__main__":
     print(f"\n\n{'='*20} {config['data']['dataset_name']} - {start_time} - {device} {'='*20}")
 
     # Load the dataset
-    data, shots =  data_modules.get_data(device=device, **config["data"])
+    data, shots =  data_modules.nc_get_data(device=device, **config["data"])
 
     # Baseline
-    functions.baseline(config, data, shots, device, results_path=config["results_path"])
+    functions.nc_baseline(config, data, shots, device, results_path=config["results_path"])
 
     # Regularization Strategies
-    functions.reg_baseline(config, data, shots, device, results_path=config["results_path"], csr=True)    # Cosine Similarity Regularization
-    functions.reg_baseline(config, data, shots, device, results_path=config["results_path"], er=True)     # Entropy Regularization
+    functions.nc_regularization(config, data, shots, device, results_path=config["results_path"], csr=True)    # Cosine Similarity Regularization
+    functions.nc_regularization(config, data, shots, device, results_path=config["results_path"], er=True)     # Entropy Regularization
 
     # Augmentation Strategies
-    functions.drop_message(config, data, shots, device, results_path=config["results_path"])                # DropMessage
-    functions.dropout(config, data, shots, device, "DropNode", results_path=config["results_path"])         # DropNode
-    functions.dropout(config, data, shots, device, "DropEdge", results_path=config["results_path"])         # DropEdge
-    functions.dropout(config, data, shots, device, "DropAttributes", results_path=config["results_path"])   # DropAttributes
-    functions.augmentation(config, data, shots, device, results_path=config["results_path"])                # Augmentation
-    functions.rewiring(config, data, shots, device, results_path=config["results_path"])                    # Rewiring
+    functions.nc_drop_message(config, data, shots, device, results_path=config["results_path"])                # DropMessage
+    functions.nc_dropblock(config, data, shots, device, "DropNode", results_path=config["results_path"])         # DropNode
+    functions.nc_dropblock(config, data, shots, device, "DropEdge", results_path=config["results_path"])         # DropEdge
+    functions.nc_dropblock(config, data, shots, device, "DropAttributes", results_path=config["results_path"])   # DropAttributes
+    functions.nc_augmentation(config, data, shots, device, results_path=config["results_path"])                # Augmentation
+    functions.nc_rewiring(config, data, shots, device, results_path=config["results_path"])                    # Rewiring
 
     # Pre-training and Fine-tuning
-    functions.graphcl(config, data, shots, device, results_path=config["results_path"])        # GraphCL
+    functions.nc_graphcl(config, data, shots, device, results_path=config["results_path"])        # GraphCL
 
     # Prototypical Networks
-    functions.protonet(config, data, shots, device, results_path=config["results_path"])       # Prototypical Networks
+    functions.nc_protonet(config, data, shots, device, results_path=config["results_path"])       # Prototypical Networks
 
     # Siamese Networks
-    functions.siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='PW')        # Pairwise Loss
-    functions.siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='PW_A')      # Pairwise Loss with Augmentation
-    functions.siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='TPL')       # Triplet Loss
-    functions.siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='TPL_A')     # Triplet Loss with Augmentation
+    functions.nc_siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='PW')        # Pairwise Loss
+    functions.nc_siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='PW_A')      # Pairwise Loss with Augmentation
+    functions.nc_siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='TPL')       # Triplet Loss
+    functions.nc_siamesenet(config, data, shots, device, results_path=config["results_path"], CL_Loss='TPL_A')     # Triplet Loss with Augmentation
 
     end_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"\n\n{'='*20} {config['data']['dataset_name']} - {end_time} - {device} {'='*20}\n\n")
